3d6220a77cbed4d9a1cc807df8f3e7a768e5ec41
Eiji Iwazawa <iwazawa@gmail.com>
2013-05-03 19:21:49 +0900

    set UserAgent

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,5 +1,6 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    NSUserDefaults.standardUserDefaults.registerDefaults("UserAgent" => "Mozilla/5.0 (iPhone; CPU iPhone OS 5_1 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 iwazer.MyBrowser/0.1")
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
 
     @web_view_controller = WebViewController.new
